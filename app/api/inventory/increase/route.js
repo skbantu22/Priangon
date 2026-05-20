0c8b46e6a765fe5cf03e5ca3504b1af943bd30fb
skbantu22 <itsantu1@gmail.com.com>
2026-05-21 01:22:42 +0600

    change inventory

--- app/api/inventory/increase/route.js
+++ app/api/inventory/increase/route.js
@@ -1,4 +1,4 @@
-import { connectDB } from "@/lib/db";
+import { connectDB } from "@/lib/databaseconnection";
 import { increaseStock } from "@/lib/inventoryService";
 
 export async function POST(req) {
